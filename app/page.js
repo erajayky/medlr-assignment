c788a54f7cbef303e31b752841081810a8007eca
Ajay <aky11052003@gmail.com>
2024-05-31 16:22:03 +0530

    pagination done

--- app/page.js
+++ app/page.js
@@ -1,29 +1,55 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import useFetch from "./useFetch";
+import useFetch from "./hooks/useFetch";
+import Pagination from "@/components/Pagination";
 
 export default function Home() {
  
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentData, setCurrentData] = useState([]);
+  
   const { fetchCsvData } = useFetch();
-
+  
   useEffect(() => {
     fetchCsvData('./output.csv', setData);
   },[]);
+  useEffect(()=>{
+    setCurrentData(data.slice(startIndex, startIndex + itemsPerPage));
+  },[data])
 
-console.log(data);
+  const handlePageChange = (page) => {
+    handle();
+    setCurrentPage(page);
+  };
+  const itemsPerPage = 10;
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+
+
+  const handle = () => {
+    setCurrentData(data.slice(startIndex, startIndex + itemsPerPage));
+  }
+
+// console.log(data);
 
   return ( 
     <div>
         namaskar nextjs
         <h1>Data</h1>
-        {data.map((medicine, idx) => (
-          <div key={medicine.index}>
-          <p className=" text-red-300">{idx}</p>
-          <p>{medicine.Manufacturer}</p>
-          </div>
+        <h1>Paginated List</h1>
+      <ul>
+        {currentData.map(item => (
+          <li key={item.id}>{item.Medicine_Name}</li>
         ))}
+      </ul>
+      <Pagination
+        totalItems={data.length}
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
---
+++ components/Pagination.jsx
@@ -0,0 +1,40 @@
+const Pagination = ({
+  totalItems,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handlePageChange = (page) => {
+    onPageChange(page);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          key={index + 1}
+          onClick={() => handlePageChange(index + 1)}
+          disabled={currentPage === index + 1}
+        >
+          {index + 1}
+        </button>
+      ))}
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
